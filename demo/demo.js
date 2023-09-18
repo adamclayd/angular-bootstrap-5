96b7c95276ff67340eb27ba0346122bab73b8565
Adam <adamclayd@gmail.com>
2023-09-18 10:55:26 -0500

    Seperated it into their own module files, added a build script, took out the requirement bootstrap javascript library requirement, took out the ng animate requirement and added my own javascript animations

--- demo/demo.js
+++ demo/demo.js
@@ -1,6 +1,7 @@
 import { faker } from 'https://cdn.skypack.dev/@faker-js/faker';
 
-angular.module('demo', ['ngBootstrap5'])
+console.log('loaded demo');
+angular.module('app', ['ngBootstrap5'])
 
     .controller('MainController', ['$scope', '$bs5Modal', function($scope,  $bs5Modal) {
         $scope.faker = faker;
